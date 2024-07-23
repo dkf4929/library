0a9c7cf6675c9fa250039ceacc21cb2f2775d8f2
dkf4929 <sng4929@naver.com>
2024-07-23 12:03:13 +0900

    feat: 카테고리 entity 생성

---
+++ src/main/java/com/project/library/domain/category/Category.java
@@ -0,0 +1,37 @@
+package com.project.library.domain.category;
+
+import com.project.library.domain.bookCategory.BookCategory;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    // 카테고리명
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<BookCategory> bookCategories = new ArrayList<>();
+
+    public void addBookCategory(BookCategory bookCategory) {
+        this.bookCategories.add(bookCategory);
+    }
+}
