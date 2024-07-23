1a0b128c9457a8a17320db70061389808b5cb317
dkf4929 <sng4929@naver.com>
2024-07-23 11:58:24 +0900

    feat: 초기 데이터

---
+++ src/main/resources/data.sql
@@ -0,0 +1,47 @@
+INSERT INTO author (name) VALUES ('권태영');
+INSERT INTO author (name) VALUES ('현영서');
+INSERT INTO author (name) VALUES ('장동혁');
+INSERT INTO author (name) VALUES ('홍길동');
+INSERT INTO author (name) VALUES ('이서연');
+INSERT INTO author (name) VALUES ('위성원');
+INSERT INTO author (name) VALUES ('지승열');
+INSERT INTO author (name) VALUES ('장지명');
+INSERT INTO author (name) VALUES ('이승열');
+
+INSERT INTO category (name) VALUES ('문학');
+INSERT INTO category (name) VALUES ('경제경영');
+INSERT INTO category (name) VALUES ('인문학');
+INSERT INTO category (name) VALUES ('IT');
+INSERT INTO category (name) VALUES ('과학');
+
+INSERT INTO book (author_id, title, is_available) VALUES (1, '너에게 해주지 못한 말들', true);
+INSERT INTO book (author_id, title, is_available) VALUES (2, '단순하게 배부르게', true);
+INSERT INTO book (author_id, title, is_available) VALUES (1, '게으른 사랑', true);
+INSERT INTO book (author_id, title, is_available) VALUES (1, '트랜드 코리아 2322', true);
+INSERT INTO book (author_id, title, is_available) VALUES (3, '초격자 투자', true);
+INSERT INTO book (author_id, title, is_available) VALUES (4, '파이어족 강환국의 하면 되지 않는다! 퀀트 투자', true);
+INSERT INTO book (author_id, title, is_available) VALUES (5, '진심보다 밥', true);
+INSERT INTO book (author_id, title, is_available) VALUES (6, '실패에 대하여 생각하지 마라', true);
+INSERT INTO book (author_id, title, is_available) VALUES (7, '실리콘밸리 리더십 쉽다', true);
+INSERT INTO book (author_id, title, is_available) VALUES (7, '데이터분석을 위한 A 프로그래밍', true);
+INSERT INTO book (author_id, title, is_available) VALUES (3, '인공지능1-12', true);
+INSERT INTO book (author_id, title, is_available) VALUES (6, '-1년차 게임 개발', true);
+INSERT INTO book (author_id, title, is_available) VALUES (1, 'Skye가 알려주는 피부 채색의 비결', true);
+INSERT INTO book (author_id, title, is_available) VALUES (8, '자연의 발전', true);
+INSERT INTO book (author_id, title, is_available) VALUES (9, '코스모스 필 무렵', true);
+
+INSERT INTO book_category (book_id, category_id) VALUES (1, 1);
+INSERT INTO book_category (book_id, category_id) VALUES (2, 1);
+INSERT INTO book_category (book_id, category_id) VALUES (3, 1);
+INSERT INTO book_category (book_id, category_id) VALUES (4, 2);
+INSERT INTO book_category (book_id, category_id) VALUES (5, 2);
+INSERT INTO book_category (book_id, category_id) VALUES (6, 2);
+INSERT INTO book_category (book_id, category_id) VALUES (7, 3);
+INSERT INTO book_category (book_id, category_id) VALUES (8, 3);
+INSERT INTO book_category (book_id, category_id) VALUES (9, 4);
+INSERT INTO book_category (book_id, category_id) VALUES (10, 4);
+INSERT INTO book_category (book_id, category_id) VALUES (11, 4);
+INSERT INTO book_category (book_id, category_id) VALUES (12, 4);
+INSERT INTO book_category (book_id, category_id) VALUES (13, 4);
+INSERT INTO book_category (book_id, category_id) VALUES (14, 5);
+INSERT INTO book_category (book_id, category_id) VALUES (15, 5);
